978763bb4b68fe39f5c34efdda8864ba64c78b62
siedd <saidmwacharo8@gmail.com>
2025-01-17 16:04:50 +0300

    users

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -28,6 +28,7 @@ const Users: React.FC<UsersPageProps> = ({ users }) => {
   );
 };
 
+// `getStaticProps` should be defined outside of the component
 export const getStaticProps = async () => {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
