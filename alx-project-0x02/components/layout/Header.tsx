b785633a6f4165fa33b6cbab11cddb2fc53b4fbb
siedd <saidmwacharo8@gmail.com>
2025-01-17 15:02:23 +0300

    interface

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header className="bg-blue-500 text-white p-4">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold">ALX Project 0x02</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/home">
+                <a className="hover:text-gray-300">Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a className="hover:text-gray-300">About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
 
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,1 +1,11 @@
+import React from 'react';
+const About = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold">Welcome to the About Page</h1>
+    </div>
+  );
+};
+
+export default About;
 
